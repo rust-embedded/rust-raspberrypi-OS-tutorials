8a3f6f5f15e5eb9a7af867ff82384a6b2e4ed181
Andre Richter <andre.o.richter@gmail.com>
2018-06-17 13:20:25 +0200

    utils: Exit early on build failure

--- utils/make_all.rb
+++ utils/make_all.rb
@@ -8,6 +8,9 @@ crates.each do |x|
   x = File.dirname(x)
   puts "\n\n" + x.to_s + "\n\n"
   Dir.chdir(x) do
-    `make`
+    if system('make') != 0
+      puts "\n\nBuild failed!"
+      exit(1) # Exit with error code
+    end
   end
 end
