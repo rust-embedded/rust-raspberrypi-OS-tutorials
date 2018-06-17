99f4dc24b342dff6b16df69e6fdf56a4327c5ede
Andre Richter <andre.o.richter@gmail.com>
2018-06-17 13:14:03 +0200

    Change dockcross path to newest upstream release

--- utils/make_all.rb
+++ utils/make_all.rb
@@ -3,8 +3,9 @@
 crates = Dir["**/Cargo.toml"]
 
 crates.each do |x|
-  x = File.dirname(x)
+  next if x.include?('raspi3_glue')
 
+  x = File.dirname(x)
   puts "\n\n" + x.to_s + "\n\n"
   Dir.chdir(x) do
     `make`
