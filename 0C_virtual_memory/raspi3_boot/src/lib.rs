2a4fdc4eced4a6b97bd4c55fe16d5589e16a0a33
Andre Richter <andre.o.richter@gmail.com>
2019-01-13 14:14:21 +0100

    Add missing comment that triggered build errors in new nightly rust.
    Also, update binaries generated by new nightly version:
    rustc 1.33.0-nightly (75a369c5b 2019-01-12)

--- 0C_virtual_memory/raspi3_boot/src/lib.rs
+++ 0C_virtual_memory/raspi3_boot/src/lib.rs
@@ -2,7 +2,7 @@
  * MIT License
  *
  * Copyright (c) 2018 Jorge Aparicio
- * Copyright (c) 2018 Andre Richter <andre.o.richter@gmail.com>
+ * Copyright (c) 2018-2019 Andre Richter <andre.o.richter@gmail.com>
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -31,6 +31,7 @@
 
 extern crate panic_abort;
 
+/// Type check the user-supplied entry function.
 #[macro_export]
 macro_rules! entry {
     ($path:path) => {
