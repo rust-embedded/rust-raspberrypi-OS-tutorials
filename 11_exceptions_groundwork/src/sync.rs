f1919952f800f703a19d387eca4504ec261c6cec
Andre Richter <andre.o.richter@gmail.com>
2019-03-18 17:34:54 +0100

    Add README for tutorial 0F

--- 11_exceptions_groundwork/src/sync.rs
+++ 11_exceptions_groundwork/src/sync.rs
@@ -28,17 +28,6 @@ pub struct NullLock<T> {
     data: UnsafeCell<T>,
 }
 
-// Since we are instantiating this struct as a static variable, which could
-// potentially be shared between different threads, we need to tell the compiler
-// that sharing of this struct is safe by marking it with the Sync trait.
-//
-// At this point in time, we can do so without worrying, because the kernel
-// anyways runs on a single core and interrupts are disabled. In short, multiple
-// threads don't exist yet in our code.
-//
-// Literature:
-// https://doc.rust-lang.org/beta/nomicon/send-and-sync.html
-// https://doc.rust-lang.org/book/ch16-04-extensible-concurrency-sync-and-send.html
 unsafe impl<T> Sync for NullLock<T> {}
 
 impl<T> NullLock<T> {
@@ -55,8 +44,8 @@ impl<T> NullLock<T> {
         F: FnOnce(&mut T) -> R,
     {
         // In a real lock, there would be code around this line that ensures
-        // that this mutable reference will ever only be given out to one thread
-        // at a time.
+        // that this mutable reference will ever only be given out one at a
+        // time.
         f(unsafe { &mut *self.data.get() })
     }
 }
