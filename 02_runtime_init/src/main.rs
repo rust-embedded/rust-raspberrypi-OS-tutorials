d6accd6c6918cd91c09cc2941a4ca145bd43589a
Andre Richter <andre.o.richter@gmail.com>
2019-09-22 16:47:47 +0200

    Add code for tutorial 02

---
+++ 02_runtime_init/src/main.rs
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+//
+// Copyright (c) 2018-2019 Andre Richter <andre.o.richter@gmail.com>
+
+//! The `kernel`
+
+#![feature(asm)]
+#![feature(global_asm)]
+#![no_main]
+#![no_std]
+
+// This module conditionally includes the correct `BSP` which provides the
+// `_start()` function, the first function to run.
+mod bsp;
+
+// Afterwards, `BSP`'s early init code calls `runtime_init::init()` of this
+// module, which on completion, jumps to `kernel_entry()`.
+mod runtime_init;
+
+/// Entrypoint of the `kernel`.
+fn kernel_entry() -> ! {
+    panic!()
+}
