f9020bbb0390ce60d0f8651b222cacfa1cf01821
Andre Richter <andre.o.richter@gmail.com>
2019-09-25 11:05:22 +0200

    Add script for diffing two tutorial sources

---
+++ utils/diff_tut_folders.bash
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+DIFF=$(
+    diff -uNr \
+	 -x README.md \
+	 -x kernel \
+	 -x kernel8.img \
+	 -x Cargo.lock \
+	 -x target \
+	 $1 $2
+     )
+
+printf "\n\n## Diff to previous\n" >> "$2/README.md"
+printf "\`\`\`diff\n" >> "$2/README.md"
+printf "${DIFF//'diff -uNr -x README.md -x kernel -x kernel8.img -x Cargo.lock -x target'/'\ndiff -uNr'}" >> "$2/README.md"
+printf "\n\`\`\`\n" >> "$2/README.md"
