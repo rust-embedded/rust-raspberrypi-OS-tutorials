b74d678efc1a2fa74c181eee627fda275c3cacf1
Andre Richter <andre.o.richter@gmail.com>
2019-11-24 15:18:36 +0100

    Add code for tutorials 12

---
+++ 12_exceptions_part1/src/panic_wait.rs
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+//
+// Copyright (c) 2018-2019 Andre Richter <andre.o.richter@gmail.com>
+
+//! A panic handler that infinitely waits.
+
+use crate::{arch, bsp};
+use core::{fmt, panic::PanicInfo};
+
+fn _panic_print(args: fmt::Arguments) {
+    use fmt::Write;
+
+    unsafe { bsp::panic_console_out().write_fmt(args).unwrap() };
+}
+
+/// Prints with a newline - only use from the panic handler.
+///
+/// Carbon copy from https://doc.rust-lang.org/src/std/macros.rs.html
+#[macro_export]
+macro_rules! panic_println {
+    ($($arg:tt)*) => ({
+        _panic_print(format_args_nl!($($arg)*));
+    })
+}
+
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    if let Some(args) = info.message() {
+        panic_println!("Kernel panic: {}", args);
+    } else {
+        panic_println!("Kernel panic!");
+    }
+
+    arch::wait_forever()
+}
