5d6b68d7107ed921f046fdf45ace252f63dbcc6a
Andre Richter <andre.o.richter@gmail.com>
2021-07-04 15:26:46 +0200

    rust-analyzer: Fix error with recent version

--- 08_hw_debug_JTAG/build.rs
+++ 08_hw_debug_JTAG/build.rs
@@ -1,7 +1,7 @@
 use std::env;
 
 fn main() {
-    let linker_file = env::var("LINKER_FILE").unwrap();
+    let linker_file = env::var("LINKER_FILE").unwrap_or_default();
 
     println!("cargo:rerun-if-changed={}", linker_file);
     println!("cargo:rerun-if-changed=build.rs");
