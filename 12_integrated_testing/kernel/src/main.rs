f4f02da28ed34fa679cfb2efa2bb3ea0c89b2507
Bryan Lee <doslby@gmail.com>
2022-04-27 01:13:00 +0800

    chore: replace `git.io` link with the original URL

--- 12_integrated_testing/kernel/src/main.rs
+++ 12_integrated_testing/kernel/src/main.rs
@@ -3,7 +3,7 @@
 // Copyright (c) 2018-2022 Andre Richter <andre.o.richter@gmail.com>
 
 // Rust embedded logo for `make doc`.
-#![doc(html_logo_url = "https://git.io/JeGIp")]
+#![doc(html_logo_url = "https://raw.githubusercontent.com/rust-embedded/wg/master/assets/logo/ewg-logo-blue-white-on-transparent.png")]
 
 //! The `kernel` binary.
 
